261e33bee8b3a6d08c8de0c1098fe977e481f2e0
Talal Numan <talalnuman1@gmail.com>
2022-11-12 15:33:23 +0500

    RTI

--- 4. RomanToInteger.js
+++ 4. RomanToInteger.js
@@ -3,19 +3,27 @@ const dir = {
   V: 5,
   X: 10,
   L: 50,
+  C: 100,
   D: 500,
   M: 1000,
 };
 function romanToInteger(rom) {
   let value = 0;
   let roman = rom.toUpperCase();
-  for (let i = 0; i < roman.length; i++) {
-    let character = roman.charAt(i);
+  let prev = "";
+  const romArr = roman.split("").reverse();
+  for (let i = 0; i < romArr.length; i++) {
+    let character = romArr[i];
     if (!dir.hasOwnProperty(character)) {
       return "Enter the correct Roman Number";
     }
-    value += dir[character];
+    if (dir[character] > dir[prev] || prev === "") {
+      value += dir[character];
+    } else {
+      value -= dir[character];
+    }
+    prev = character;
   }
   return value;
 }
-console.log(romanToInteger("12"));
+console.log(romanToInteger("CMXLIX")); //1551
