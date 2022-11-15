477e13ff677e18ff78555e8f396eb808e0dda543
Talal Numan <talalnuman1@gmail.com>
2022-11-15 05:20:59 +0500

    Max Water in Container O(n)

--- 5. MaxWater.js
+++ 5. MaxWater.js
@@ -1,3 +1,4 @@
+// With O(n^2)
 var maxArea = function (height) {
   let max = 0;
   for (let i = 0; i < height.length; i++) {
@@ -10,4 +11,21 @@ var maxArea = function (height) {
   }
   return max;
 };
+
+// With O(n)
+const maxArea = (arr) => {
+  let l = 0;
+  let r = arr.length - 1;
+  let area = 0;
+
+  while (l < r) {
+    // Calculating the max area
+    area = Math.max(area, Math.min(arr[l], arr[r]) * (r - l));
+
+    if (arr[l] < arr[r]) l += 1;
+    else r -= 1;
+  }
+  return area;
+};
+
 console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
